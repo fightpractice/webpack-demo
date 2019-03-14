640bbd07df9fdc25eb93d72dd9061ac5c9b3c0bd
hehui <hui.he@risewinter.com>
2019-03-14 10:47:41 +0800

    模块热替换

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import _ from 'lodash';
 import printMe from './print.js';
+import './style.css'
 
 function component() {
     var element = document.createElement('div');
     var btn = document.createElement('button');
 
-    element.innerHTML = _.join(['hello', 'webpack','hhh','dddee'], ' ');
+    element.innerHTML = _.join(['hello', 'webpack','hhh'], ' ');
 
     btn.innerHTML = 'Click me and check the console!';
     btn.onclick = printMe;
@@ -16,4 +17,14 @@ function component() {
 
 }
 
-document.body.appendChild(component());
+let element = component();
+document.body.appendChild(element);
+
+if (module.hot) {
+    module.hot.accept('./print.js', function(){
+        console.log('Accepting the updated printMe module!');
+        document.body.removeChild(element);
+        element = component();
+        document.body.appendChild(element);
+    })
+}
